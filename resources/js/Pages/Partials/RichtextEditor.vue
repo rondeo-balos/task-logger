f6a36dca216f6ad50fd0d8a556a3a2369eba9b25
Rondeo Balos <mr.ron669@gmail.com>
2025-09-14 15:01:03 +0800

    removed pro tools

--- resources/js/Pages/Partials/RichtextEditor.vue
+++ resources/js/Pages/Partials/RichtextEditor.vue
@@ -9,10 +9,11 @@ const value = defineModel();
         :init="{
             toolbar_mode: 'sliding',
             plugins: [
-                'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-                'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'advtemplate', 'uploadcare', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
+                'anchor', 'autolink', 'charmap', 'codesample', 'emoticons',
+                'link', 'lists', 'media', 'searchreplace', 'table',
+                'visualblocks', 'wordcount'
             ],
-            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography uploadcare | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
             tinycomments_mode: 'embedded',
             tinycomments_author: 'Author name',
             mergetags_list: [
